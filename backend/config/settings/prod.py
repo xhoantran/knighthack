e43025d62b9ad268628fe5f4d3671b377454696b
Hoan Tran <xhoantran@Hoans-MacBook-Pro.local>
2021-11-13 11:03:02 -0500

    Update Staic

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -53,3 +53,6 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
     "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
 }
+
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "static")
+STATIC_URL = "/static/"
