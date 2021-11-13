eb771c4c35dbee5903f8522ae1f20209f857d238
Tyler Pearson <pearsonty9@gmail.com>
2021-11-13 11:15:24 -0500

    username dropdown for logout / settings

--- frontend/src/components/trip-panel/TripPanelControl.js
+++ frontend/src/components/trip-panel/TripPanelControl.js
@@ -1,9 +1,93 @@
-import { Box } from "@mui/material";
+import * as React from "react";
+import {
+	Box,
+	Menu,
+	MenuItem,
+	ListItemIcon,
+	Tooltip,
+	Button,
+	Typography,
+} from "@mui/material";
+import PersonAdd from "@mui/icons-material/PersonAdd";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
 
 export const TripPanelControl = () => {
-  return (
-    <Box sx={{ backgroundColor: "red", height: "5rem", mb: "4rem" }}>
-      TripPanelControl
-    </Box>
-  );
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+	return (
+		<Box sx={{ mb: 3 }}>
+			<Button
+				onClick={handleClick}
+				variant="text"
+				sx={{
+					width: "100%",
+					color: "black",
+					justifyContent: "flex-start",
+				}}
+			>
+				<Typography variant="h5">Username</Typography>
+			</Button>
+			<Menu
+				anchorEl={anchorEl}
+				open={open}
+				onClose={handleClose}
+				onClick={handleClose}
+				PaperProps={{
+					elevation: 0,
+					sx: {
+						minWidth: 300,
+						overflow: "visible",
+						filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+						mt: 1.5,
+						"& .MuiAvatar-root": {
+							width: 32,
+							height: 32,
+							ml: -0.5,
+							mr: 1,
+						},
+						"&:before": {
+							content: '""',
+							display: "block",
+							position: "absolute",
+							top: 0,
+							left: 10,
+							width: 10,
+							height: 10,
+							bgcolor: "background.paper",
+							transform: "translateY(-50%) rotate(45deg)",
+							zIndex: 0,
+						},
+					},
+				}}
+				transformOrigin={{ horizontal: "right", vertical: "top" }}
+				anchorOrigin={{ horizontal: "left", vertical: "bottom" }}
+			>
+				<MenuItem>
+					<ListItemIcon>
+						<PersonAdd fontSize="small" />
+					</ListItemIcon>
+					Add another account
+				</MenuItem>
+				<MenuItem>
+					<ListItemIcon>
+						<Settings fontSize="small" />
+					</ListItemIcon>
+					Settings
+				</MenuItem>
+				<MenuItem>
+					<ListItemIcon>
+						<Logout fontSize="small" />
+					</ListItemIcon>
+					Logout
+				</MenuItem>
+			</Menu>
+		</Box>
+	);
 };
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -4,47 +4,47 @@ import { TripPanel } from "../components/trip-panel/TripPanel";
 import { PlacePanel } from "../components/place-panel/PlacePanel";
 
 function Dashboard(props) {
-  return (
-    <div>
-      <Grid container direction="row">
-        <Grid
-          item
-          xs={6}
-          md={3}
-          direction="column"
-          sx={{ padding: "1rem", height: "100vh" }}
-        >
-          <TripPanel />
-        </Grid>
-        <Grid
-          item
-          xs={6}
-          md={4}
-          sx={{
-            padding: "1rem",
-            background: "grey",
-            color: "white",
-            height: "100vh",
-          }}
-        >
-          <PlacePanel />
-        </Grid>
-        <Grid
-          item
-          xs={6}
-          md={5}
-          sx={{
-            padding: "1rem",
-            background: "black",
-            color: "white",
-            height: "100vh",
-          }}
-        >
-          Place details
-        </Grid>
-      </Grid>
-    </div>
-  );
+	return (
+		<div>
+			<Grid container direction="row">
+				<Grid
+					item
+					xs={6}
+					md={3}
+					direction="column"
+					sx={{ padding: "1rem", height: "100vh" }}
+				>
+					<TripPanel />
+				</Grid>
+				<Grid
+					item
+					xs={6}
+					md={4}
+					sx={{
+						padding: "1rem",
+						background: "grey",
+						color: "white",
+						height: "100vh",
+					}}
+				>
+					<PlacePanel />
+				</Grid>
+				<Grid
+					item
+					xs={6}
+					md={5}
+					sx={{
+						padding: "1rem",
+						background: "black",
+						color: "white",
+						height: "100vh",
+					}}
+				>
+					Place details
+				</Grid>
+			</Grid>
+		</div>
+	);
 }
 
 export default Dashboard;
