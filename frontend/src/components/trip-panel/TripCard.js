64b2c5a5d1b4d063fd52b87a23ccb7986c4fc7b9
Aaron Nguyen <me@aaronnguyen.dev>
2021-11-13 09:17:07 -0500

    Add TripCard

---
+++ frontend/src/components/trip-panel/TripCard.js
@@ -0,0 +1,20 @@
+import { Paper, Typography } from "@mui/material";
+
+export const TripCard = () => {
+  return (
+    <Paper
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        px: 3,
+        py: 1,
+        cursor: "pointer",
+      }}
+    >
+      <Typography sx={{ fontWeight: "medium", textTransform: "uppercase" }}>
+        Name
+      </Typography>
+      <Typography>Date</Typography>
+    </Paper>
+  );
+};
