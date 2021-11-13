c85831aa38b5bc01c0bbbeeb2dd448ad030d10b0
Hoan Tran <xhoantran@Hoans-MacBook-Pro.local>
2021-11-13 16:36:58 -0500

    Change name

--- apiauth/views.py
+++ apiauth/views.py
@@ -9,7 +9,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 class BlackListTokenView(APIView):
     def post(self, request):
         try:
-            refresh_token = request.data["refresh_token"]
+            refresh_token = request.data["refresh"]
             token = RefreshToken(refresh_token)
             token.blacklist()
             return Response("Success")
