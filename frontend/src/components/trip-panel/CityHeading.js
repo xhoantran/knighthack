11aeff2646b7fe1351eb7dc32d375876371d582c
Aaron Nguyen <me@aaronnguyen.dev>
2021-11-13 09:43:31 -0500

    Add CityHeading

---
+++ frontend/src/components/trip-panel/CityHeading.js
@@ -0,0 +1,17 @@
+import { Typography } from "@mui/material";
+
+export const CityHeading = () => {
+  return (
+    <Typography
+      sx={{
+        fontWeight: "medium",
+        fontSize: "1.2rem",
+        mt: 3,
+        mb: 1,
+        textTransform: "uppercase",
+      }}
+    >
+      City
+    </Typography>
+  );
+};
