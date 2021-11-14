6c41742a83da77bd277027e1a9a71586a702163f
Hoan Tran <xhoantran@Hoans-MacBook-Pro.local>
2021-11-14 02:04:31 -0500

    Allowed cors all

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -26,9 +26,8 @@ db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 DATABASES['default']['CONN_MAX_AGE'] = 500
 
-CORS_ALLOWED_ORIGINS = [
-    "*"
-]
+# CORS_ALLOWED_ORIGINS = ["localhost:3000"]
+CORS_ORIGIN_ALLOW_ALL = True
 
 # SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SIMPLE_JWT = {
